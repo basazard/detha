d1cafafa61808e6d7f299b775ceb97482a1d196b
basazard <bagas123ft@gmail.com>
2026-09-02 03:12:22 +0700

    fixing new npm

--- src/app/(auth)/login/page.js
+++ src/app/(auth)/login/page.js
@@ -27,7 +27,7 @@ export default function MacOSLogin() {
   const handlePasscodeSubmit = (e) => {
     e.preventDefault();
 
-    if (passcode === "20042005") {
+    if (passcode === "24042005") {
       router.push("/home");
     } else {
       setError("Incorrect passcode. Please try again.");
